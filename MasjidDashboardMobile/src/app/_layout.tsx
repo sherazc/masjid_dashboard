ba96b4a9f403e05c1789abbedfb5f97c00c5a437
sheraz@mbp16 <sheraz@bitsegment.com>
2026-05-16 11:08:54 -0400

    MDB UI

--- MasjidDashboardMobile/src/app/_layout.tsx
+++ MasjidDashboardMobile/src/app/_layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout() {
           name="index" 
           options={{ headerShown: false }}
         />
+        <Stack.Screen 
+          name="prayer/[companyId]" 
+          options={{ headerShown: false }}
+        />
       </Stack>
     </Provider>
   );
