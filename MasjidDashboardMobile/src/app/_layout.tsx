de2b4abcc0518f1370f2ef131e1c6585d6d3e575
sheraz@mbp16 <sheraz@bitsegment.com>
2026-05-22 18:20:31 -0400

    MDB UI

--- MasjidDashboardMobile/src/app/_layout.tsx
+++ MasjidDashboardMobile/src/app/_layout.tsx
@@ -3,15 +3,32 @@ import { Stack } from "expo-router";
 import { ConstantsStyles } from "../services/Constants";
 import { Provider } from "react-redux";
 import store from "../store/rootReducer";
+import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
+  const customTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      background: ConstantsStyles.color.background2,
+    },
+  };
+
   return (
     <Provider store={store}>
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: ConstantsStyles.color.background2 },
-          headerTintColor: ConstantsStyles.text.colorLight,
-        }}>
+      <ThemeProvider value={customTheme}>
+        {/* <StatusBar> sets top bar where time, batter and network bars are shown */}
+        <StatusBar style="light" animated={true}/>
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: ConstantsStyles.color.background2 },
+            headerTintColor: ConstantsStyles.text.colorLight,
+            contentStyle: { backgroundColor: ConstantsStyles.color.background2 },
+            // headerShadowVisible: false,
+            // headerTransparent: false,
+            // headerBlurEffect: "none",
+          }}>
 
         <Stack.Screen
           name="index"
@@ -40,6 +57,7 @@ export default function RootLayout() {
           name="company/company-select"
           options={{ headerShown: false }} />
       </Stack>
+      </ThemeProvider>
     </Provider>
   );
 }
--- MasjidDashboardMobile/src/app/settings/_layout.tsx
+++ MasjidDashboardMobile/src/app/settings/_layout.tsx
@@ -1,9 +1,18 @@
 import { Stack } from "expo-router";
+import { ConstantsStyles } from "../../services/Constants";
 
 export default function SettingsLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: ConstantsStyles.color.background2 },
+        headerTintColor: ConstantsStyles.text.colorLight,
+        contentStyle: { backgroundColor: ConstantsStyles.color.background2 },
+        // headerShadowVisible: false,
+        // headerTransparent: false,
+        // headerBlurEffect: "none",
+      }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
